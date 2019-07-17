2ce8b111503c6868a6370e35ec368f1b13c4f628
gwangil <pki054@naver.com>
2019-07-18 04:08:07 +0900

    1. DB 접속 예외 처리 추가 2. 초기 실행 시, GPIO 핀 맵핑 가지고 오는 부분 추가

--- soeasy/configure.py
+++ soeasy/configure.py
@@ -2,8 +2,19 @@ import logging
 from soeasy import dbhandler
 
 class Configure(dbhandler.DBHandler):
+    hardware = ""
+
     def __init__(self):
+        dbhandler.DBHandler.__init__(self)
         logging.info("Get Configuration")
 
     def get_gpio_list(self):
+        self.cursor.execute("SELECT * FROM GPIO_LIST", "")
+        result = self.cursor.fetchall()
+
+        for record in result:
+            logging.info(record)
+
         logging.info("Get GPIO List")
+
+        return result
